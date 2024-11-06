d985065f14edc81630e22df41c917fc77a38b21d
Sarthak1207 <sarthak.sharma1207@gmail.com>
2024-11-06 11:26:52 +0530

    updating ip in mongodb

--- roboshop-shell/mongodb.sh
+++ roboshop-shell/mongodb.sh
@@ -5,8 +5,10 @@ cp /home/ec2-user/Roboshop_Project/roboshop-shell/mongo.repo /etc/yum.repos.d/
 #install mongodb
 dnf install mongodb-org -y
 
+#update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+
 #start and enable mongodb service
 systemctl enable mongod
 systemctl restart mongod
 
-#update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf

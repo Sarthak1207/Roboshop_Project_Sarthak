5feb6f3e3e824f64fce565de0d9cb07659f3afd4
Sarthak1207 <sarthak.sharma1207@gmail.com>
2024-11-06 10:29:50 +0530

    Fixing the nginx spelling

--- roboshop-shell/frontend.sh
+++ roboshop-shell/frontend.sh
@@ -1,22 +1,22 @@
-#Install Ngnix
-dnf module disable ngnix -y
-dnf module enable ngnix:1.24 -y
-dnf install ngnix -y
+#Install nginx
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
+dnf install nginx -y
 
 #Extract the frontend content
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 #Remove teh default contant that web server is serving
-rm -rf /usr/share/ngnix/html/*
+rm -rf /usr/share/nginx/html/*
 
 #Download the frontend content
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 
-#Create Ngnix reverese proxy configuration to each backend service
+#Create nginx reverese proxy configuration to each backend service
 #nginx file is place in git under roboshop project
 cp /home/ec2-user/Roboshop_Project/roboshop-shell/nginx.conf /etc/nginx/
 
-#Start and Enable Ngnix service
+#Start and Enable nginx service
 systemctl enable nginx
 systemctl start nginx

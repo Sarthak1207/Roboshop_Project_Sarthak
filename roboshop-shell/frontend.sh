30b913ce9d83e22cb826a0c7917160e5af6f7893
Sarthak1207 <sarthak.sharma1207@gmail.com>
2024-11-06 10:24:28 +0530

    Updating frontend.sh

--- roboshop-shell/frontend.sh
+++ roboshop-shell/frontend.sh
@@ -3,9 +3,9 @@ dnf module disable ngnix -y
 dnf module enable ngnix:1.24 -y
 dnf install ngnix -y
 
-#Start and Enable Ngnix service
-systemctl enable nginx
-systemctl start nginx
+#Extract the frontend content
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
 
 #Remove teh default contant that web server is serving
 rm -rf /usr/share/ngnix/html/*
@@ -13,13 +13,10 @@ rm -rf /usr/share/ngnix/html/*
 #Download the frontend content
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 
-#Extract the frontend content
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-
 #Create Ngnix reverese proxy configuration to each backend service
 #nginx file is place in git under roboshop project
-vim /etc/nginx/nginx.conf
+cp /home/ec2-user/Roboshop_Project/roboshop-shell/nginx.conf /etc/nginx/
 
-#Restart Nginx Service to load the changes of the configuration
-systemctl restart nginx
+#Start and Enable Ngnix service
+systemctl enable nginx
+systemctl start nginx

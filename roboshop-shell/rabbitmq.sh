51a57478e04d6845e69acdc1c8ae03bd1760b3a6
Sarthak1207 <sarthak.sharma1207@gmail.com>
2024-11-09 00:45:36 +0530

    updating rabittmq

--- roboshop-shell/rabbitmq.sh
+++ roboshop-shell/rabbitmq.sh
@@ -4,10 +4,10 @@ cp /home/ec2-user/Roboshop_Project/roboshop-shell/rabbitmq.repo /etc/yum.repos.d
 #Install rabbitmq
 dnf install rabbitmq-server -y
 
-#updating username and password for rabbitmq to connect
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
-
 #start rabbitmq service
 systemctl enable rabbitmq-server
 systemctl restart rabbitmq-server
+
+#updating username and password for rabbitmq to connect
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"

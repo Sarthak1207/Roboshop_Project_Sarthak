e56831c0d3efa5d5e4ec1ad3312a2eea199be189
Sarthak1207 <sarthak.sharma1207@gmail.com>
2024-11-08 14:34:27 +0530

    Fixing frontend

--- roboshop-shell/frontend.sh
+++ roboshop-shell/frontend.sh
@@ -3,15 +3,15 @@ dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
 
-#Extract the frontend content
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-
 #Remove teh default contant that web server is serving
 rm -rf /usr/share/nginx/html/*
 
 #Download the frontend content
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+
+#Extract the frontend content
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
 
 #Create nginx reverese proxy configuration to each backend service
 #nginx file is place in git under roboshop project

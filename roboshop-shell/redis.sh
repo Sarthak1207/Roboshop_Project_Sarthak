9b66077a81618af734b3c9db13db8123df2fa46f
Sarthak1207 <sarthak.sharma1207@gmail.com>
2024-11-29 13:42:53 +0530

    updating protected mode in redis.sh

--- roboshop-shell/redis.sh
+++ roboshop-shell/redis.sh
@@ -5,6 +5,7 @@ dnf install redis -y
 
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis/redis.conf
 sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
+sed -i -e '/protected-mode/s/yes/no/g' /etc/redis/redis.conf
 
 #Start and enable redis service
 systemctl enable redis

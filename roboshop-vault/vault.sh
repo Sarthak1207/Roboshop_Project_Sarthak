8609dee27d6b88cd1b483cebbd8158e777a4c63b
Sarthak1207 <sarthak.sharma1207@gmail.com>
2024-12-31 13:16:56 +0530

    Vault changes

--- roboshop-vault/vault.sh
+++
@@ -1,5 +0,0 @@
-sudo -i
-set-prompt vault
-curl -L -o /etc/yum.repos.d/vault.repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-sudo yum -y install vault
-systemctl start vault (run as root user)

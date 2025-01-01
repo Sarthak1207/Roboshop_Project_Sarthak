eb7093a093451f3c1b4786038d7126324249d210
Sarthak1207 <sarthak.sharma1207@gmail.com>
2025-01-01 15:20:28 +0530

    Vault infra

--- tool_setup_roboshop/module-ec2/main.tf
+++ tool_setup_roboshop/module-ec2/main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "instance" {
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
-    Name  =    "var.tool_name"
+    Name  =    var.tool_name
   }
   root_block_device {
     volume_size = var.volume_size

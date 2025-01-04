a18379f2bec68729361f5a289dd4a2e380440add
Sarthak1207 <sarthak.sharma1207@gmail.com>
2025-01-04 14:45:47 +0530

    Multiple changes for Vault secrets

--- tool_setup_roboshop/misc/vault_secrets/main.tf
+++ tool_setup_roboshop/misc/vault_secrets/main.tf
@@ -57,9 +57,9 @@ resource "vault_generic_secret" "user" {
     path = "${vault_mount.roboshop-dev.path}/user" 
     data_json = <<EOT
 {
-    MONGO=true
-    REDIS_URL='redis://redis-{{ env }}.sarthak1207.shop:6379'
-    MONGO_URL="mongodb://mongodb-{{ env }}.sarthak1207.shop:27017/users"
+    "MONGO": "true"
+    "REDIS_URL": "redis://redis-dev.sarthak1207.shop:6379"
+    "MONGO_URL": "mongodb://mongodb-dev.sarthak1207.shop:27017/users"
 }
 EOT
 }
@@ -68,8 +68,8 @@ resource "vault_generic_secret" "cart" {
     path = "${vault_mount.roboshop-dev.path}/cart" 
     data_json = <<EOT
 {
-    REDIS_HOST=redis-{{ env }}.sarthak1207.shop
-    CATALOGUE_HOST=catalogue-{{ env }}.sarthak1207.shop
+    "REDIS_HOST": "redis-dev.sarthak1207.shop"
+    "CATALOGUE_HOST": "catalogue-dev.sarthak1207.shop"
 }
 EOT
 }
@@ -78,13 +78,13 @@ resource "vault_generic_secret" "payment" {
     path = "${vault_mount.roboshop-dev.path}/payment" 
     data_json = <<EOT
 {
-    CART_HOST=cart-{{ env }}.sarthak1207.shop
-    CART_PORT=8080
-    USER_HOST=user-{{ env }}.sarthak1207.shop
-    USER_PORT=8080
-    AMQP_HOST=rabbitmq-{{ env }}.sarthak1207.shop
-    AMQP_USER=roboshop
-    AMQP_PASS=roboshop123
+    "CART_HOST": "cart-dev.sarthak1207.shop"
+    "CART_PORT": "8080"
+    "USER_HOST": "user-dev.sarthak1207.shop"
+    "USER_PORT": "8080"
+    "AMQP_HOST": "rabbitmq-dev.sarthak1207.shop"
+    "AMQP_USER": "roboshop"
+    "AMQP_PASS": "roboshop123"
 }
 EOT
 }
@@ -93,9 +93,9 @@ resource "vault_generic_secret" "shipping" {
     path = "${vault_mount.roboshop-dev.path}/shipping" 
     data_json = <<EOT
 {
-    CART_ENDPOINT=cart-{{ env }}.sarthak1207.shop:8080
-    DB_HOST=mysql-{{ env }}.sarthak1207.shop
-    MYSQL_ROOT_PASSWORD=RoboShop@1
+    "CART_ENDPOINT": "cart-dev.sarthak1207.shop:8080"
+    "DB_HOST": "mysql-dev.sarthak1207.shop"
+    "MYSQL_ROOT_PASSWORD": "RoboShop@1"
 }
 EOT
 }
@@ -104,7 +104,17 @@ resource "vault_generic_secret" "mysql" {
     path = "${vault_mount.roboshop-dev.path}/mysql" 
     data_json = <<EOT
 {
-    MYSQL_ROOT_PASSWORD=RoboShop@1
+    "MYSQL_ROOT_PASSWORD": "RoboShop@1"
+}
+EOT
+}
+
+resource "vault_generic_secret" "rabbitmq" {
+    path = "${vault_mount.roboshop-dev.path}/rabbitmq" 
+    data_json = <<EOT
+{
+    "user": "roboshop"
+    "password": "roboshop123"
 }
 EOT
 }
